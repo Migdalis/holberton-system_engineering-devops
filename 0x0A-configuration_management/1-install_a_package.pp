01d0c5087c9bebfe67163ab4b66cbd7aee16c5fd
Migdalis <miglepage@gmail.com>
2022-04-19 14:04:52 -0500

    Add change task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 # Install a package
 
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem',
-  source   => 'https://rubygems.org/',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
