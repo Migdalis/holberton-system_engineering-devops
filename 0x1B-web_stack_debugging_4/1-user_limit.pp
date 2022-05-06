4ed53b9583a28e6b3a70739f775d0f70a6862694
Migdalis <miglepage@gmail.com>
2022-05-05 21:32:26 -0500

    :hammer: Add task 1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,6 @@
+# Fix error Too Many Open Files nginx to user holberton
+
+exec { 'change-limits':
+  command  => "sed -i 's/holberton hard nofile 5/holberton hard nofile 30000/' /etc/security/limits.conf && sed -i 's/holberton soft nofile 4/holberton soft nofile 30000/' /etc/security/limits.conf",
+  provider => shell
+}
