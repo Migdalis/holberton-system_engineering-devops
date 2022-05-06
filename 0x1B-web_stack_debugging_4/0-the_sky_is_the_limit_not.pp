2e602907f67d24ac4ebacea64b1b0acfa2ab81ae
Migdalis <miglepage@gmail.com>
2022-05-05 20:51:03 -0500

    :hammer: Simplify task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,6 @@
 # Fix error Too Many Open Files nginx
 
 exec { 'worker_processes':
-  command  => "sed -i 's/worker_processes 4;/worker_processes 7;/' /etc/nginx/nginx.conf",
-  provider => shell
-} ->
-
-exec { 'nginx-restart':
-  command  => 'sudo service nginx start',
+  command  => "sed -i 's/worker_processes 4;/worker_processes 7;/' /etc/nginx/nginx.conf && sudo service nginx start",
   provider => shell
 }
