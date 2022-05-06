39111755ab10bb478f5149f74c0e184007a2e1db
Migdalis <miglepage@gmail.com>
2022-05-05 20:57:47 -0500

    :hammer: Try some task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,16 +1,6 @@
 # Fix error Too Many Open Files nginx
 
 exec { 'worker_processes':
-  command  => "sed -i 's/worker_processes 4;/worker_processes 7;/' /etc/nginx/nginx.conf",
-  provider => shell
-} ->
-
-exec { 'multi-accept-on':
-  command  => "sed -i 's/# multi_accept on;/multi_accept on;/' /etc/nginx/nginx.conf",
-  provider => shell
-} ->
-
-exec { 'nginx-restart':
-  command  => 'service nginx restart',
+  command  => "sed -i 's/worker_processes 4;/worker_processes 7;/' /etc/nginx/nginx.conf && sudo service nginx restart",
   provider => shell
 }
