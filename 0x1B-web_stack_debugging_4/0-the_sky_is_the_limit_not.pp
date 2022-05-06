e999c78f3ae7d308776b13d4cc029b9e34d9697e
Migdalis <miglepage@gmail.com>
2022-05-05 20:54:20 -0500

    :hammer: Change start to restart task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,16 @@
 # Fix error Too Many Open Files nginx
 
 exec { 'worker_processes':
-  command  => "sed -i 's/worker_processes 4;/worker_processes 7;/' /etc/nginx/nginx.conf && sudo service nginx start",
+  command  => "sed -i 's/worker_processes 4;/worker_processes 7;/' /etc/nginx/nginx.conf",
+  provider => shell
+} ->
+
+exec { 'multi-accept-on':
+  command  => "sed -i 's/# multi_accept on;/multi_accept on;/' /etc/nginx/nginx.conf",
+  provider => shell
+} ->
+
+exec { 'nginx-restart':
+  command  => 'service nginx restart',
   provider => shell
 }
