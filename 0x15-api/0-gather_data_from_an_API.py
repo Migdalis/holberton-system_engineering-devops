cb1af05f2bc0ee04037f4180ad29ec05639b399c
Migdalis <miglepage@gmail.com>
2022-05-19 21:51:26 -0500

    :white_check_mark: Add Readme and task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Script that returns information about
+TODO list progress for a given employee ID"""
+import sys
+import requests
+
+
+if len(sys.argv) == 2:
+    try:
+        id = int(sys.argv[1])
+        list_tasks = []
+        total_tasks = 0
+        url = "https://jsonplaceholder.typicode.com/todos"
+        response = requests.get(url).json()
+        for todo in response:
+            if todo['userId'] == id:
+                total_tasks += 1
+                if todo['completed'] is True:
+                    list_tasks.append("\t " + todo['title'])
+        url = "https://jsonplaceholder.typicode.com/users/" + sys.argv[1]
+        employee = requests.get(url).json()
+        print("Employee {} is done with tasks({}/{}):".format(
+            employee['name'], len(list_tasks), total_tasks))
+        print("\n".join(list_tasks))
+    except Exception:
+        exit()
